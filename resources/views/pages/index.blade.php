3c4cb48602bdfcc2d52539e1aff0f60189ab605e
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-13 16:19:28 +0100

    create card

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,15 @@
         <h1>Treni: {{ count($trains) }}</h1>
         @foreach ($trains as $train)
             <div class="card">
-                {{$train->company}}
+                <h6>Azienda: {{ $train->company }}</h6>
+                <h6>Partenza: {{ $train->departure_station }}</h6>
+                <h6>Arrivo: {{ $train->arrival_station }}</h6>
+                <h6>Partenza: {{ $train->departure_time }}</h6>
+                <h6>Arrivo: {{ $train->arrival_time }}</h6>
+                <h6>Codice treno: {{ $train->train_code }}</h6>
+                <h6>Numero carrozze: {{ $train->number_carriages }}</h6>
+                <h6>In orario: {{ $train->on_time ? 'Sì' : 'No' }}</h6>
+                <h6>Cancellato: {{ $train->cancelled ? 'Sì' : 'No' }}</h6>
             </div>
         @endforeach
     </div>
